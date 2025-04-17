863db2c3905d95921e73b9aeecfde6569094cde1
mauroanastasi <mauroanastasi1@gmail.com>
2025-04-17 16:06:42 +0200

    [FIX] problema in rotta posts.js, test su postman ok

--- routers/posts.js
+++ routers/posts.js
@@ -5,32 +5,32 @@ const express = require(`express`);
 const router = express.Router();
 
 // index    
-router.get('/posts', function (req, res) {
+router.get('/', function (req, res) {
     res.send('Lista dei posts');
 });
     
 // show
-router.get('/posts/:id', function (req, res) {
+router.get('/:id', function (req, res) {
     res.send('Dettagli dei posts ' + req.params.id);
 });
     
 // store
-router.post('/posts', function (req, res) {
+router.post('/', function (req, res) {
     res.send('Creazione nuovi posts');
 });
     
 // update
-router.put('/posts/:id', function (req, res) {
+router.put('/:id', function (req, res) {
     res.send('Modifica integrale dei posts ' + req.params.id);
 });
     
 // modify
-router.patch('/posts/:id', function (req, res) {
+router.patch('/:id', function (req, res) {
     res.send('Modifica parziale dei posts ' + req.params.id);
 });
     
 // destroy
-router.delete('/posts/:id', function (req, res) {
+router.delete('/:id', function (req, res) {
     res.send('Eliminazione dei posts ' + req.params.id);
 });
 
