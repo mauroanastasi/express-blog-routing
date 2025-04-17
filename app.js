9f9975f5de91b9f98637189cab3ed7c817f2eb56
mauroanastasi <mauroanastasi1@gmail.com>
2025-04-17 15:11:58 +0200

    [UPD] definisco prima rotta, metto in ascolto server su porta indicata, testo su portman

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 // importiamo express
-const express = require (`express`);
+const express = require(`express`);
 
 // inizializzo express invocandolo come funzione e salvando il risultato in una variabile
 const app = express();
+
+// definisco la porta
+const port = 3000;
+
+// definisco la prima rotta
+app.get(`/`, (req,res) => {
+    res.send(`hello world`)
+});
+
+// avvio il server mettendolo in ascolto della porta definita
+app.listen(port,() => {
+    console.log(`Example app listening on port ${port}`)
+});
