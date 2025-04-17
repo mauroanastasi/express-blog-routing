367a7878deaf40e37b46ffff90a08868d72476a7
mauroanastasi <mauroanastasi1@gmail.com>
2025-04-17 15:26:03 +0200

    [UPD] importo cartella imgs, posts.js, definisco asset statici

--- app.js
+++ app.js
@@ -16,3 +16,6 @@ app.get(`/`, (req,res) => {
 app.listen(port,() => {
     console.log(`Example app listening on port ${port}`)
 });
+
+// definisco gli asset statici
+app.use(express.static(`public`))
