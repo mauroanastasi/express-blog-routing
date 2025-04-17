0943fde46649fa4b7b2efb91ba50177f539a139e
mauroanastasi <mauroanastasi1@gmail.com>
2025-04-17 15:44:26 +0200

    [UPD] creazione singole rotte

--- routers/posts.js
+++ routers/posts.js
@@ -4,3 +4,33 @@ const express = require(`express`);
 // importo la classe router
 const router = express.Router();
 
+// index    
+router.get('/posts', function (req, res) {
+    res.send('Lista dei posts');
+});
+    
+// show
+router.get('/posts/:id', function (req, res) {
+    res.send('Dettagli dei posts ' + req.params.id);
+});
+    
+// store
+router.post('/posts', function (req, res) {
+    res.send('Creazione nuovi posts');
+});
+    
+// update
+router.put('/posts/:id', function (req, res) {
+    res.send('Modifica integrale dei posts ' + req.params.id);
+});
+    
+// modify
+router.patch('/posts/:id', function (req, res) {
+    res.send('Modifica parziale dei posts ' + req.params.id);
+});
+    
+// destroy
+router.delete('/posts/:id', function (req, res) {
+    res.send('Eliminazione dei postsa ' + req.params.id);
+});
+
